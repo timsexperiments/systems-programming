0fdfe12c2f63541b3f147bf3de9589bb7e826815
Tim <76897954+timsexperiments@users.noreply.github.com>
2023-04-23 07:25:53 +0000

    fix linked list implementation

--- learning/c/linked-list/main.c
+++ learning/c/linked-list/main.c
@@ -4,7 +4,6 @@
 typedef struct node
 {
     struct node *next;
-    struct node *prev;
     int value;
 } node;
 
@@ -12,213 +11,95 @@ typedef void (*list_callback)(struct node *, void *args);
 
 struct list
 {
-    struct node **head;
-    struct node **tail;
+    struct node *head;
+    struct node *tail;
 };
 
 struct list *list_init();
-void list_push_stack(struct list *l, int value);
-void list_push_queue(struct list *l, int value);
-void node_print(struct node *node, void *);
-void list_print(struct list *l);
-void list_destack(struct list *l, int *val);
-void list_dequeue(struct list *l, int *val);
-void node_disconnect(struct node *node, int *val);
-void list_remove(struct list *l, int value, int *val);
-void list_foreach(struct list *l, list_callback callback, void *args);
-void list_free(struct list *l);
-void node_free(struct node *node);
+void list_free(struct list);
+void list_add(struct list *, int value);
+void list_remove(struct list *, int search_value);
+void list_pop_queue(struct list *);
+void list_pop_stack(struct list *);
+struct node *node_init();
+void list_print(struct list);
+void list_foreach(struct list, list_callback callback, void *args);
 
 int main()
 {
-    // Initialize a new list
-    printf("Initializing list...\n");
-    struct list *l = list_init();
-    printf("List: ");
-    list_print(l);
-    printf("\n");
+    // Test empty list initialization
+    struct list *list = list_init();
+    list_print(*list);
 
-    // Push some elements onto the stack
-    printf("Pushing elements onto the stack...\n");
-    list_push_stack(l, 1);
-    list_push_stack(l, 2);
-    list_push_stack(l, 3);
-    printf("List: ");
-    list_print(l);
-    printf("\n");
+    // Test removing from empty list
+    list_remove(list, 4);
+    list_print(*list);
 
-    // Push some elements onto the queue
-    printf("Pushing elements onto the queue...\n");
-    list_push_queue(l, 4);
-    list_push_queue(l, 5);
-    list_push_queue(l, 6);
-    printf("List: ");
-    list_print(l);
-    printf("\n");
+    // Test popping from empty list
+    list_pop_queue(list);
+    list_print(*list);
+    list_pop_stack(list);
+    list_print(*list);
 
-    // Dequeue some elements from the queue
-    printf("Dequeueing elements from the queue...\n");
-    int *val1 = malloc(sizeof(int));
-    list_dequeue(l, val1);
-    int *val2 = malloc(sizeof(int));
-    list_dequeue(l, val2);
-    int *val3 = malloc(sizeof(int));
-    list_dequeue(l, val3);
-    printf("Dequeued values: %d, %d, %d\n", *val1, *val2, *val3);
-    printf("List: ");
-    list_print(l);
-    printf("\n");
+    // Test adding and removing single element
+    list_add(list, 4);
+    list_print(*list);
+    list_remove(list, 4);
+    list_print(*list);
 
-    // Destack some elements from the stack
-    printf("Destacking some elements from the stack...\n");
-    int *val4 = malloc(sizeof(int));
-    list_destack(l, val4);
-    int *val5 = malloc(sizeof(int));
-    list_destack(l, val5);
-    printf("Dequeued values: %d, %d\n", *val4, *val5);
-    printf("List: ");
-    list_print(l);
-    printf("\n");
+    // Test adding and popping single element
+    list_add(list, 4);
+    list_print(*list);
+    list_pop_queue(list);
+    list_print(*list);
+    list_add(list, 4);
+    list_print(*list);
+    list_pop_stack(list);
+    list_print(*list);
 
-    // Remove an element from the list
-    printf("Removing an element from the list...\n");
-    int *removed_val = malloc(sizeof(int));
-    list_remove(l, 2, removed_val);
-    printf("Removed value: %p\n", removed_val);
-    printf("List: ");
-    list_print(l);
-    printf("\n");
+    // Test adding and removing multiple elements
+    list_add(list, 4);
+    list_add(list, 6);
+    list_add(list, 3);
+    list_add(list, 8);
+    list_add(list, 0);
+    list_add(list, 3);
+    list_add(list, 4);
+    list_print(*list);
+    list_remove(list, 3);
+    list_print(*list);
+    list_remove(list, 4);
+    list_print(*list);
+    list_remove(list, 4);
+    list_print(*list);
 
-    // Free the list
-    printf("Freeing the list...\n");
-    list_free(l);
-    printf("List: ");
-    list_print(l);
-    printf("\n");
-    free(l);
-    free(val1);
-    free(val2);
-    free(val3);
-    free(val4);
-    free(val5);
-    free(removed_val);
-    return 0;
+    // Test popping entire list
+    list_pop_queue(list);
+    list_pop_queue(list);
+    list_pop_queue(list);
+    list_pop_queue(list);
+    list_pop_queue(list);
+    list_pop_queue(list);
+    list_pop_queue(list);
+    list_print(*list);
+
+    // Test freeing list
+    list_free(*list);
+    free(list);
 }
 
 struct list *list_init()
 {
-    struct list *l = malloc(sizeof(struct list));
-    l->head = malloc(sizeof(struct node *));
-    l->tail = malloc(sizeof(struct node *));
-    *l->head = NULL;
-    *l->tail = NULL;
-    return l;
+    struct list *list = malloc(sizeof(struct list));
+    list->head = NULL;
+    list->tail = NULL;
+    return list;
 }
 
-void list_push_stack(struct list *l, int value)
+void node_free(struct node *);
+void list_free(struct list list)
 {
-    struct node *node = malloc(sizeof(struct node));
-    node->value = value;
-    node->next = NULL;
-    node->prev = NULL;
-    if (*l->head == NULL)
-    {
-        *l->tail = node;
-    }
-    else
-    {
-        node->next = *l->head;
-        (*l->head)->prev = node;
-    }
-    *l->head = node;
-}
-
-void list_push_queue(struct list *l, int value)
-{
-    struct node *node = malloc(sizeof(struct node));
-    node->value = value;
-    node->next = NULL;
-    node->prev = NULL;
-    if (l->tail == NULL)
-    {
-        *l->head = node;
-    }
-    else
-    {
-        node->prev = *l->tail;
-        (*l->tail)->next = node;
-    }
-    *l->tail = node;
-}
-
-void list_destack(struct list *l, int *val)
-{
-    struct node *head = *l->head;
-    if (head == NULL)
-    {
-        val = NULL;
-        return;
-    }
-    head->next->prev = NULL;
-    l->head = head->next;
-    head->next = NULL;
-    *val = head->value;
-    node_free(head);
-}
-
-void list_dequeue(struct list *l, int *val)
-{
-    struct node *tail = l->tail;
-    if (tail == NULL)
-    {
-        val = NULL;
-        return;
-    }
-    tail->prev->next = NULL;
-    *l->tail = tail->prev;
-    tail->prev = NULL;
-    *val = tail->value;
-    free(tail);
-}
-
-void node_print(struct node *node, void *args)
-{
-    printf("%d", node->value);
-    if (node->next != NULL)
-    {
-        printf(", ");
-    }
-}
-
-void list_print(struct list *l)
-{
-    printf("[");
-    list_foreach(l, node_print, NULL);
-    printf("]\n");
-}
-
-void list_free(struct list *l)
-{
-    if (l->head == NULL)
-    {
-        return;
-    }
-    if (*l->head != NULL)
-    {
-        node_free(*l->head);
-        (*l->head) = NULL;
-        (*l->tail) = NULL;
-    }
-    if (l->head != NULL)
-    {
-        free(l->head);
-        l->head = NULL;
-    }
-    if (l->tail != NULL)
-    {
-        free(l->tail);
-        l->tail = NULL;
-    }
+    node_free(list.head);
 }
 
 void node_free(struct node *node)
@@ -227,103 +108,186 @@ void node_free(struct node *node)
     {
         return;
     }
-    if (node->prev != NULL)
-    {
-        node_free(node->next);
-    }
-    if (node->prev != NULL)
-    {
-        node_free(node->prev);
-    }
-    if (node != NULL)
-    {
-        free(node);
-        node = NULL;
-    }
-}
-
-void node_to_end(struct node *node, list_callback callback, void *args);
-void list_foreach(struct list *l, list_callback callback, void *args)
-{
-    if (l->head != NULL)
-    {
-        node_to_end(*l->head, callback, args);
-    }
-}
-void node_to_end(struct node *node, list_callback callback, void *args)
-{
-    if (node != NULL)
-    {
-        callback(node, args);
-        node_to_end(node->next, callback, (void *)args);
-    }
-}
-
-void node_disconnect(struct node *node, int *val)
-{
-    if (node == NULL)
-    {
-        val = NULL;
-        return;
-    }
-    *val = node->value;
-    if (node->prev != NULL)
-    {
-        node->prev->next = node->next;
-    }
     if (node->next != NULL)
     {
-        node->next->prev = node->prev;
+        node_free(node->next);
+        node->next = NULL;
     }
-    node->next = NULL;
-    node->prev = NULL;
-    node_free(node);
+    free(node);
+    node = NULL;
 }
 
-struct check_value_args
+struct node *node_init()
 {
-    int **value;
-    int matches;
-    int search_val;
-};
+    struct node *node = malloc(sizeof(struct node));
+    node->next = NULL;
+    return node;
+}
 
-void node_remove_check(struct node *node, void *args);
-void list_remove(struct list *l, int value, int *val)
+void list_add(struct list *list, int value)
 {
-    struct check_value_args *args = malloc(sizeof(struct check_value_args));
-    args->value = malloc(sizeof(int *));
-    *args->value = val;
-    args->matches = 0;
-    args->search_val = value;
-    list_foreach(l, node_remove_check, (void *)args);
-    printf("Found %d matches\n", args->matches);
-    free(args->value);
-    args->value = NULL;
+    struct node *node = node_init();
+    node->value = value;
+    if (list->head == NULL && list->tail == NULL)
+    {
+        list->head = node;
+        if (list->tail != NULL)
+        {
+            node_free(list->tail);
+            list->tail = NULL;
+        }
+        list->tail = node;
+    }
+    else
+    {
+        list->tail->next = node;
+        list->tail = node;
+    }
+}
+
+void list_pop_stack(struct list *list)
+{
+    if (list->head == list->tail)
+    {
+        struct node *old_head = list->head;
+        list->head = NULL;
+        list->tail = NULL;
+        node_free(old_head);
+    }
+    if (list->head == NULL)
+    {
+        return;
+    }
+    struct node *old_head = list->head;
+    list->head = list->head->next;
+    old_head->next = NULL;
+    node_free(old_head);
+    old_head = NULL;
+}
+
+struct node_remove_next_args
+{
+    int num_removed;
+    int search_value;
+};
+void node_remove_next(struct node *, void *);
+void list_remove(struct list *list, int search_value)
+{
+    if (list->head == NULL)
+    {
+        return;
+    }
+    if (list->head->value == search_value)
+    {
+        struct node *old_head = list->head;
+        if (list->head == list->tail)
+        {
+            list->tail = NULL;
+        }
+        list->head = list->head->next;
+        old_head->next = NULL;
+        node_free(old_head);
+        old_head = NULL;
+        return;
+    }
+    struct node_remove_next_args *args = malloc(sizeof(struct node_remove_next_args));
+    args->num_removed = 0;
+    args->search_value = search_value;
+    list_foreach(*list, node_remove_next, (void *)args);
     free(args);
     args = NULL;
 }
-void node_remove_check(struct node *node, void *args)
+
+void list_pop_queue_recursive(struct node *node);
+void list_pop_queue(struct list *list)
 {
-    struct check_value_args *options = (struct check_value_args *)args;
+    if (list->head == list->tail)
+    {
+        struct node *old_head = list->head;
+        list->head = NULL;
+        list->tail = NULL;
+        node_free(old_head);
+        return;
+    }
+    list_pop_queue_recursive(list->head);
+}
+void list_pop_queue_recursive(struct node *node)
+{
     if (node == NULL)
     {
         return;
     }
+    if (node->next == NULL)
+    {
+        return;
+    }
+    if (node->next->next == NULL)
+    {
+        struct node *node_to_remove = node->next;
+        node->next = NULL;
+        node_free(node_to_remove);
+        node_to_remove = NULL;
+    }
+}
 
+void node_remove_next(struct node *node, void *args)
+{
+    struct node_remove_next_args *options = (struct node_remove_next_args *)args;
+    if (options->num_removed > 0)
+    {
+        return;
+    }
     if (options == NULL)
     {
         return;
     }
-
-    if (options->matches != 0)
+    if (node == NULL)
     {
         return;
     }
-
-    if (options->search_val == node->value)
+    if (node->next == NULL)
     {
-        printf("DISCONNECTING....\n");
-        node_disconnect(node, *options->value);
-        options->matches++;
+        return;
     }
+    if (node->next->value == options->search_value)
+    {
+        struct node *node_to_remove = node->next;
+        node->next = node->next->next;
+        node_to_remove->next = NULL;
+        node_free(node_to_remove);
+        options->num_removed++;
+    }
+}
+
+void node_print(struct node *, void *args);
+void list_print(struct list list)
+{
+    printf("list{ head = %p, tail = %p }[", list.head, list.tail);
+    list_foreach(list, node_print, NULL);
+    printf("]\n");
+}
+
+void node_print(struct node *node, void *args)
+{
+    printf("{ value = %d, next = %p }", node->value, node->next);
+    if (node->next != NULL)
+    {
+        printf(", ");
+    }
+}
+
+void list_foreach_recursive(struct node *current, list_callback callback, void *args);
+void list_foreach(struct list list, list_callback callback, void *args)
+{
+    list_foreach_recursive(list.head, callback, args);
+}
+
+void list_foreach_recursive(struct node *current, list_callback callback, void *args)
+{
+    if (current == NULL)
+    {
+        return;
+    }
+    callback(current, args);
+    list_foreach_recursive(current->next, callback, args);
 }
